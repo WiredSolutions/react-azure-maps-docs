17cf42d6b3c7d52780d53a08cba4a84e94a3392d
psrednicki <patryk.srednicki@acaisoft.com>
2021-03-23 22:09:47 +0100

    feat: update next and update sample

--- pages/live-example-map/components/mapDynamicComponent.js
+++ pages/live-example-map/components/mapDynamicComponent.js
@@ -1,45 +1,58 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 
 const option = {
     authOptions: {
-        authType: 'subscriptionKey',
-    },
-}
-
+        authType: "subscriptionKey"
+    }
+};
 export default function MapComponent() {
-    const [module, setModule] = useState(null)
-    const [key, setKey] = useState()
+    const reactMapRef = useRef(null);
+    const [key, setKey] = useState("");
+    const forceUpdate = useReducer(() => ({}), {})[1];
 
     useEffect(() => {
-        import('react-azure-maps').then((module) => {
-            setModule(module)
+        import("react-azure-maps").then(module => {
+            reactMapRef.current = module;
+            forceUpdate();
         });
-    }, [])
+    }, []);
 
     return (
         <div>
-            {!key && <div>
-                <form onSubmit={v => {
-                    setKey(v.target[0].value)
-                }
-                }>
-                    <label htmlFor="key">Subscription Key</label>
-                    <input id="key" name="key" type="text" autoComplete="azure-maps-key" required/>
-                    <button type="submit">Add key</button>
-                </form>
-            </div>}
-            {!!module && key && (
+            {!key && (
+                <div>
+                    <form
+                        onSubmit={v => {
+                            setKey(v.target[0].value);
+                        }}
+                    >
+                        <label htmlFor="key">Subscription Key  </label>
+                        <input
+                            id="key"
+                            name="key"
+                            type="text"
+                            autoComplete="azure-maps-key"
+                            required
+                        />
+                        <button type="submit">Add key</button>
+                    </form>
+                </div>
+            )}
+            {!!reactMapRef.current && key && (
                 <div>
                     <p>Module is ready</p>
-                    <div style={{height: '300px'}}>
-                        <module.AzureMapsProvider>
-                            <module.AzureMap options={{option, ...{...option.authType, ...{subscriptionKey: key}}}}>
-                            </module.AzureMap>
-                        </module.AzureMapsProvider>
+                    <div style={{ height: "300px" }}>
+                        <reactMapRef.current.AzureMapsProvider>
+                            <reactMapRef.current.AzureMap
+                                options={{
+                                    option,
+                                    ...{ ...option.authType, ...{ subscriptionKey: key } }
+                                }}
+                            />
+                        </reactMapRef.current.AzureMapsProvider>
                     </div>
                 </div>
             )}
         </div>
-    )
+    );
 }
-
--- pages/live-example-map/index.js
+++ pages/live-example-map/index.js
@@ -4,9 +4,8 @@ import dynamic from 'next/dynamic'
 
 const DynamicComponentWithNoSSR = dynamic(
     () => import('./components/mapDynamicComponent'),
-    { loading: () => <p>Loading React azure maps ...</p>, ssr: false }
+    {loading: () => <p>Loading React azure maps ...</p>, ssr: false}
 )
-
 
 
 const LiveExample = () => {
@@ -14,65 +13,71 @@ const LiveExample = () => {
         <div>
             <Head>
                 <title>Live Map example</title>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main className="page">
                 <div className="page__content">
-                    <h1>Next.JS  Map Example</h1>
+                    <h1>Next.JS Map Example</h1>
                     <p>React-azure-maps with NEXT.JS</p>
 
-                    {process.browser &&  <DynamicComponentWithNoSSR />}
+                    <DynamicComponentWithNoSSR/>
 
                     <h2 id="usage">Usage:</h2>
                     <p>
-                       Use dynamic import with  <code>ssr: false</code> option when importing component that contains map.
+                        Use dynamic import with <code>ssr: false</code> option when importing component that contains
+                        map.
                     </p>
                     <p className="tip">
                         Map component working on as client side - so you need to import is without SSR.
                     </p>
 
                     <h2 id="code">Code:</h2>
-                    <a href={'https://github.com/WiredSolutions/react-azure-maps-playground/blob/master/src/examples/DefaultMap.tsx'} > Source Code </a>
+                    <a href={'https://github.com/WiredSolutions/react-azure-maps-docs/tree/master/pages/live-example-map/index.js'} > Source Code </a>
+                    <pre>
+                         <code className="language-jsx">
+                             {`const DynamicComponentWithNoSSR = dynamic(
+    () => import('./components/mapDynamicComponent'),
+    {loading: () => <p>Loading React azure maps ...</p>, ssr: false}
+)`}
+                         </code>
+                    </pre>
                     <pre>
                         <code className="language-jsx">
-                            {`import { useEffect, useState } from "react";
+                            {`import { useEffect, useReducer, useRef } from "react";
 
 const option = {
-    authOptions: {
-        authType: 'subscriptionKey',
-        subscriptionKey: ''// Your subscription key
-    }
-}
+  authOptions: {
+    authType: "subscriptionKey",
+    subscriptionKey: "" // Your subscription key
+  }
+};
 
 export default function MapComponent() {
-    const [module, setModule] = useState(null)
+  const reactMapRef = useRef(null);
+  const forceUpdate = useReducer(() => ({}), {})[1];
 
-    useEffect(() => {
-        import('react-azure-maps').then((module) => {
-            setModule(module)
-        });
-    }, [])
+  useEffect(() => {
+    import("react-azure-maps").then(module => {
+      reactMapRef.current = module;
+      forceUpdate();
+    });
+  }, []);
 
-    return (
+  return (
+    <div>
+      {!!reactMapRef.current && (
         <div>
-            {!!module && (
-                <div>
-
-                <p>Module is ready</p>
-                    <div style={{height: '300px'}}>
-                        <module.AzureMapsProvider>
-                            <module.AzureMap options={option}>
-                            </module.AzureMap>
-                        </module.AzureMapsProvider>
-                    </div>
-
-                </div>
-            )}
+          <p>Module is ready</p>
+          <div style={{ height: "300px" }}>
+            <reactMapRef.current.AzureMapsProvider>
+              <reactMapRef.current.AzureMap options={option} />
+            </reactMapRef.current.AzureMapsProvider>
+          </div>
         </div>
-        )
-}
-
-`}
+      )}
+    </div>
+  );
+}`}
                         </code>
                     </pre>
                 </div>
